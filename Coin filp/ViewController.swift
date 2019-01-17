b103c2503fa7128c9756632d9e0641540844bc04
Your Name <you@example.com>
2019-01-17 15:09:21 -0700

    Initial Commit

---
+++ Coin filp/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Coin filp
+//
+//  Created by David Ashieze on 2019-01-17.
+//  Copyright © 2019 David Ashieze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var headOrTails: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    
+    
+    
+    
+    @IBAction func buttonToFlip(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
