01d282b7860c8f5256fc8c5a771fb054aed56a70
Your Name <you@example.com>
2019-01-21 15:21:42 -0700

    finished with rest button

--- Coin filp/ViewController.swift
+++ Coin filp/ViewController.swift
@@ -11,26 +11,66 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var headOrTails: UILabel!
+    @IBOutlet weak var score1: UILabel!
+    @IBOutlet weak var score2: UILabel!
+    
     
   var fliped = ["Head", "Talis"]
     var randomNumber : Int = 1
+    var headScore : Int = 0
+    var tailScore : Int = 0
+    var num = Int(arc4random_uniform(2))
+    
+    
+    func scoreChange() {
+        
+        print(num)
+        
+        if num == 0 {
+            headScore += 1
+            score1.text = "\(headScore)"
+            
+            
+        }else if num == 1 {
+            tailScore += 1
+            score2.text = "\(tailScore)"
+            
+        }
+        
+        
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+       
     }
     
     @IBAction func buttonToFlip(_ sender: UIButton) {
-         let num = Int(arc4random_uniform(2))
+        num = Int(arc4random_uniform(2))
 
         randomNumber = num
         
         headOrTails.text = fliped[num]
         
-        print(randomNumber)
+        //print(randomNumber)
+       
+        scoreChange()
+    
+    }
+
+    
         
+    
+    
+    @IBAction func RestButton(_ sender: UIButton) {
+        headScore = 0
+        score1.text = "0"
+        
+        tailScore = 0
+        score2.text = "0"
     }
     
     
@@ -46,16 +86,8 @@ class ViewController: UIViewController {
     
     
     
-    
-    
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+
 
 
 }
-
